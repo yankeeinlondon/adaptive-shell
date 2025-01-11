e2117ca188ba07e4691788e6acb810d5549c61de
Ken Snyder <ken@ken.net>
2025-01-11 10:09:58 -0800

    batman

---
+++ ask.sh
@@ -0,0 +1,381 @@
+#!/usr/bin/env bash
+
+# shellcheck source="./shared.sh"
+source "./shared.sh"
+
+function default_validator () {
+    return 0;
+}
+
+# exit_ask <msg> <is_error>
+function exit_ask() {
+    local -r msg="${1:-Goodbye}"
+
+    clear
+    log ""
+    echo "${msg}"
+    
+    exit 0
+}
+
+function text_confirm() {
+    local -r question="${1:?text_confirm() did not get the question passed to it}"
+    local -r default="${2:-y}"
+    local response
+
+    if [[ $(lc "${default}") == "y" ]]; then
+        read -rp "${question} (Y/n)" response >/dev/null
+    else
+        read -rp "${question} (y/N)" response >/dev/null
+    fi
+
+    if [[ $(lc "$default") == "y" ]];then
+        # local outcome=
+        local -i resp
+        if [[ $(lc "$response") == "n" ]] || [[ $(lc "$response") == "no" ]]; then
+            resp=1
+        else
+            resp=0
+        fi        
+
+        debug "text_confirm" "question '${question}'='${response}', with ${ITALIC}default${RESET} of 'yes' -> '$(yes_no "$resp")'"
+
+        return $resp
+
+    else
+        local -i resp
+        if [[ $(lc "$response" == "y") ]] || [[ $(lc "$response") == "yes" ]]; then
+            resp=0
+        else
+            resp=1
+        fi
+
+        debug "text_confirm" "question '${question}'='${response}', with ${ITALIC}default${RESET} of 'no' -> '$(yes_no "$resp")'"
+
+        return $resp
+    fi
+}
+
+# ask
+#
+# determines which TUI to use
+function ask() {
+    if has_command "whiptail"; then
+        debug "ask" "using whiptail for TUI"
+        whiptail "${@}"
+    elif has_command "dialog"; then
+        debug "ask" "using Display for TUI"
+        display "${@}"
+    fi
+}
+
+function tui() {
+    local var
+
+    if has_command "whiptail"; then
+        var="whiptail"
+    elif has_command "dialog"; then
+        var="dialog"
+    else 
+        var="none"
+    fi
+
+    echo "${var}"
+}
+
+# ask_password <obj[ title, backmsg, height, width, list   ]  >
+#
+# 
+function ask_radiolist() {
+    # shellcheck disable=SC2178
+    local -n data=$1
+
+    if ! is_bound data; then
+        panic "Called for a radiolist dialog and didn't bind a configuration object to the request!"
+    fi
+
+    local -r title="${data["title"]}"
+    local -r backmsg="${data["backmsg"]:-Moxy}"
+    local -r height="${data["height"]:-23}"
+    local -r width="${data["width"]:-60}" 
+    local -r radio_height="${data["radio_height"]:-12}"
+    local -ra choices="${data["choices"]}"
+    local -r exit_msg="${data["exit_msg"]:-Goodbye}"
+    local -ra params=(
+        "--backtitle \"${backmsg}\""
+        "--radiolist \"${title}\" ${height} ${width} ${radio_height}"
+        "${choices[*]} 3>&2 2>&1 1>&3"
+    )
+
+    local -r tool="$(tui)"
+
+    if [[ "$tool" == "dialog" ]]; then 
+        cmd="dialog ${params[*]}"
+    elif [[ "$tool" == "whiptail" ]]; then
+        cmd="whiptail ${params[*]}" 
+    else
+        error "can't ask for password as no TUI is available [${tool}]"
+        exit 1
+    fi
+    debug "ask_radiolist" "asking for radiolist with: ${cmd}"
+    local -r choice=$(eval "$cmd") || exit_ask "$exit_msg"
+
+    echo "${choice}"
+}
+
+# ask_yes_no() <[ title, backmsg, height, width, yes, no ]>
+#
+# creates a dialog box with a yes/no dialog
+function ask_yes_no() {
+    # shellcheck disable=SC2178
+    local -n data=$1
+    local -r title="${data["title"]}"
+    local -r backmsg="${data["backmsg"]}"
+    local -r height="${data["height"]}"
+    local -r width="${data["width"]}" 
+    local -r yes="${data["yes"]}"
+    local -r no="${data["no"]}"
+    local -r exit_msg="${data["exit_msg"]:-Goodbye}"
+    local -ra params=(
+        "--backtitle \"${backmsg}\""
+        "--yesno \"${title}\" ${height} ${width}"
+        "3>&2 2>&1 1>&3"
+    )
+
+    local -r tool="$(tui)"
+
+    if [[ "$tool" == "dialog" ]]; then 
+        cmd="dialog ${params[*]}"
+    elif [[ "$tool" == "whiptail" ]]; then
+        cmd="whiptail ${params[*]}" 
+    else
+        error "can't ask for password as no TUI is available [${tool}]"
+        exit 1
+    fi
+    debug "ask_yes_no" "asking confirmation on: ${title}"
+    eval "$cmd"
+
+    return $?
+}
+
+# ask_password <title> <height> <width> <ok> <cancel> <exit>
+function ask_password() {
+    local -r title="${1:?No title provided to ask_yes_no()}"
+    local -r height="${2:?No height provided to ask_yes_no()}"
+    local -r width="${3:?No width provided to ask_yes_no()}"
+    local -r ok_btn="${4:-Ok}"
+    local -r cancel_btn="${5:-Cancel}"
+    local -r exit_msg="${6:-Goodbye}"
+    local -r tool="$(tui)"
+    local choice
+
+    if [[ "$tool" == "dialog" ]]; then 
+        choice=$(
+            dialog --ok-label "${ok_btn}" --cancel-label "${cancel_btn}" --passwordbox "${title}" "${height}" "${width}"  3>&2 2>&1 1>&3
+        ) || exit_ask "${exit_msg}" "false"
+    elif [[ "$tool" == "whiptail" ]]; then 
+        choice=$(
+            whiptail --ok-btn "${ok_btn}" --cancel-btn "${cancel_btn}" --passwordbox "${title}" "${height}" "${width}"  3>&2 2>&1 1>&3
+        ) || exit_ask "${exit_msg}" "false"
+    else
+        error "can't ask for password as no TUI is available [${tool}]"
+        exit 1
+    fi
+
+    if [ "${#choice}" -lt 8 ]; then
+        log ""
+        log ""
+        error "The key you passed in was too short [${#choice}], please refer to the Promox docs for how to generate the key and test it out with Postman or equivalent if you're unsure if it's right"
+        log ""
+        exit 1
+    else 
+        echo "${choice}"
+    fi
+}
+
+function was_cancelled() {
+    local -r outcome="$1"
+
+    if not_empty "$outcome" && [[ "$outcome" == "${CANCELLED}" ]]; then
+        return 0
+    else 
+        return 1
+    fi
+}
+
+# ask_inputbox {title|height|width|ok|cancel|exit}
+function ask_inputbox() {
+    allow_errors
+    # shellcheck disable=SC2178
+    local -n data=$1
+    catch_errors
+
+    if ! is_bound data; then
+        panic "Call to ask_inputbox() received without any parameter configuration passed in!" 1
+    elif is_not_typeof data "assoc-array"; then
+        panic "ask_inputbox() expects an associative array to be passed in for configuration purposes but got '$(typeof data)' instead!"
+        exit 1
+    fi
+
+    local -r title="${data["title"]}"
+    local -r backmsg="${data["backmsg"]}"
+    local -r height="${data["height"]}"
+    local -r width="${data["width"]}" 
+    local -r ok="${data["ok"]}"
+    local -r cancel="${data["cancel"]}"
+    local -r on_cancel="${data["on_cancel"]:-exit}"
+    local -r exit_msg="${data["exit_msg"]:-Goodbye}"
+    
+    local -r tool="$(tui)"
+    catch_errors
+
+    local buttons
+    if [[ "${tool}" == "dialog" ]]; then
+        buttons="--ok-label \"${ok}\" --cancel-label \"${cancel}\""
+    else
+        buttons="--ok-btn \"${ok}\" --cancel-btn \"${cancel}\""
+    fi
+    local -ra params=(
+        "--backtitle \"${backmsg}\""
+        "${buttons}"
+        "--inputbox \"${title}\" ${height} ${width}"
+        "3>&2 2>&1 1>&3"
+    )
+
+
+    if [[ "$tool" == "dialog" ]]; then 
+        cmd=$(eval "dialog ${params[*]} || echo ${CANCELLED}")
+    elif [[ "$tool" == "whiptail" ]]; then
+        cmd=$(whiptail "${params[*]}") || "${CANCELLED}"
+    else
+        error "can't ask for password as no TUI is available [${tool}]"
+        exit 1
+    fi
+
+    if was_cancelled "${cmd}"; then
+        if [[ "$on_cancel" == "exit" ]]; then
+            clear
+            exit_ask "${exit_msg}"
+        else
+            echo "${CANCELLED}"
+            return 0
+        fi
+    fi
+    
+    printf "%s" "$cmd"
+}
+
+
+# ask_to_continue <continue msg> <exit msg> <y|n> <err>
+function ask_to_continue() {
+    # shellcheck disable=SC2178
+    local -n data=$1
+    local -r continue_msg="${data["continue"}]}"
+    local -r exit_msg="${data["exit_msg"}]}"
+    local -r def_val="${data["def_val"}]}"
+
+
+    if [[ "$def_val" == "y" ]]; then 
+        read -r -p "Continue? <Y/n> " prompt
+        if echo "$prompt" | grep -Eq "^(n|no)$"; then
+            log "${exit_msg}"
+            exit_ask "${exit_msg}"
+        else
+            log "${continue_msg}"
+            log ""
+        fi
+
+    else
+        read -r -p "Continue? <y/N> " prompt
+        if echo "$prompt" | grep -Eq "^(y|yes)$"; then
+            log "${exit_msg}"
+            exit 1
+        else
+            log "${continue_msg}"
+            log ""
+        fi
+    fi
+}
+
+
+# ask_for_text <id> <title> <question> <[height]> <[width]> <[def_value]> <[validator fn]>
+#
+# interactively asks user for a text input and returns key/value pair to the
+# "answers" variable which it assumes to be an existingly scoped local variable
+#
+# by default any text provided by the user is considered valid but the caller may
+# pass in a validator function to limit this.
+function ask_for_text() {
+    local -r title="${1:?no title passed to ask_for_text()}"
+    local -r question="${2:?no question text passed to ask_for_text()}"
+    local -r height="${3:8}"
+    local -r width="${4:60}"
+    local -r def_value="${5:-}"
+    local -rf validate="${6:-$default_validator}"
+
+    if ui_availability; then
+        local -r ui_pkg="$(ui_availability)"
+        debug "ask_for_text" "\"${title}\" to be asked for text input by \"${ui_pkg}\""
+
+        allow_errors
+        def_value=$(${ui_pkg} --inputbox \""${question}"\" "${height}" "${width}" "${def_value}" --title "${title}" 3>&1 1>&2 2>&3)
+        local -r cmd_response=$?
+        catch_errors
+
+        if ! ${cmd_response}; then
+            debug "ask_for_text" "user opted to cancel the process"
+            # shellcheck disable=SC2086
+            return ${ERR_USER_CANCEL}
+        fi
+
+    else
+        debug "ask_for_text" "\"${title}\" to be asked for text input in plain text"
+        def_value=$(read -r -p "${BOLD}${title}${RESET}\n${question} [${value}]")
+    fi
+
+    if "$validate" "${value}"; then
+        debug "ask_for_text" "valid text being returned: ${value}"
+        echo "${value}"
+        return 0
+    else
+        debug "ask_for_text" "invalid text: ${value}"
+        # shellcheck disable=SC2086
+        return ${ERR_INVALID_TEXT_RESPONSE}
+    fi
+}
+
+function ask_from_menu() {
+    local -r title="${1:?no title passed to ask_for_text()}"
+    local -r question="${2:?no question text passed to ask_for_text()}"
+    local -r choices="${3:?no choices provided to ask_for_menu}"
+
+    local -r num_choices=iterable "count"
+    local dialog_height=$(( num_options+2 ))
+    if [[ dialog_height -gt 20 ]]; then
+        dialog_height=$(( 20 ))
+    fi
+    local -r height="${3:8}"
+    local -r width="${4:60}"
+    local -r def_value="${5:-}"
+    local -r validate="${6:-$default_validator}"
+}
+
+function ask_select_one() {
+    local -r answers="${1:the dictionary of answers was not provided to ask_select_one}"
+    local -r question="${2:the question text was not provided to ask_select_one}"
+    local -r options="${3:the list of options was not provided to ask_select_one}"
+    local -r value="${4:-$(first "$answers")}"
+
+    echo "TODO"
+}
+
+function ask_select_many() {
+    local -rA answers="${1:the dictionary of answers was not provided to ask_select_many}"
+    local -r question="${2:the question text was not provided to ask_select_many}"
+    local -r options="${3:the list of options was not provided to ask_select_many}"
+    local default_value="${4}"
+
+    echo "TODO"
+}
+
+
---
+++ shared.sh
@@ -0,0 +1,407 @@
+export RESET='\033[0m'
+export GREEN='\033[38;5;2m'
+export RED='\033[38;5;1m'
+export YELLOW2='\033[38;5;3m'
+export BLACK='\033[30m'
+export RED='\033[31m'
+export GREEN='\033[32m'
+export YELLOW='\033[33m'
+export BLUE='\033[34m'
+export MAGENTA='\033[35m'
+export CYAN='\033[36m'
+export WHITE='\033[37m'
+export BRIGHT_BLACK='\033[90m'
+export BRIGHT_RED='\033[91m'
+export BRIGHT_GREEN='\033[92m'
+export BRIGHT_YELLOW='\033[93m'
+export BRIGHT_BLUE='\033[94m'
+export BRIGHT_MAGENTA='\033[95m'
+export BRIGHT_CYAN='\033[96m'
+export BRIGHT_WHITE='\033[97m'
+
+export BOLD='\033[1m'
+export NO_BOLD='\033[21m'
+export DIM='\033[2m'
+export NO_DIM='\033[22m'
+export ITALIC='\033[3m'
+export NO_ITALIC='\033[23m'
+export STRIKE='\033[9m'
+export NO_STRIKE='\033[29m'
+export REVERSE='\033[7m'
+export NO_REVERSE='\033[27m'
+
+export ANDROID_HOME="${HOME}/Library/Android/sdk"
+export NDK_HOME="${ANDROID_HOME}/nd/$(ls -1 ${ANDROID_HOME}/ndk)"
+
+
+# log
+#
+# Logs the parameters passed to STDERR
+function log() {
+    printf "%b\\n" "${*}" >&2
+}
+
+# debug <fn> <msg> <...>
+# 
+# Logs to STDERR when the DEBUG env variable is set
+# and not equal to "false".
+function debug() {
+    local -r DEBUG=$(lc "${DEBUG:-}")
+    if [[ "${DEBUG}" != "false" ]]; then
+        if (( $# > 1 )); then
+            local fn="$1"
+
+            shift
+            local regex=""
+            local lower_fn="" 
+            lower_fn=$(lc "$fn")
+            regex="(.*[^a-z]+|^)$lower_fn($|[^a-z]+.*)"
+
+            if [[ "${DEBUG}" == "true" || "${DEBUG}" =~ $regex ]]; then
+                log "       ${GREEN}◦${RESET} ${BOLD}${fn}()${RESET} → ${*}"
+            fi
+        else
+            log "       ${GREEN}DEBUG: ${RESET} → ${*}"
+        fi
+    fi
+}
+
+# error <msg>
+#
+# sends a formatted error message to STDERR
+function error() {
+    local -r msg="${1:?no message passed to error()!}"
+    local -ri code=$(( "${2:-1}" ))
+    local -r fn="${3:-${FUNCNAME[1]}}"
+
+    log "\n  [${RED}x${RESET}] ${BOLD}ERROR ${DIM}${RED}$code${RESET}${BOLD} →${RESET} ${msg}" && return $code
+}
+
+# lc() <str>
+#
+# converts the passed in <str> to lowercase
+function lc() {
+    local -r str="${1-}"
+    echo "${str}" | tr '[:upper:]' '[:lower:]'
+}
+
+# is_numeric() <candidate>
+#
+# returns 0/1 based on whether <candidate> is numeric
+function is_numeric() {
+    local -r val="$1"
+        if ! [[ "$val" =~ ^[0-9]+$ ]]; then
+            return 1
+        else
+            return 0
+        fi
+}
+
+function is_string() {
+  local -r val="$1"
+  if [[ -z "$val" ]]; then
+    return 1
+  else
+    if [[ "$val" =~ ^[a-zA-Z]{1}.*$ ]]; then
+      return 0
+    else
+      return 1
+    fi
+  fi
+}
+
+function has_characters() {
+    local -r char_str="${1:?has_characters() did not recieve a CHARS string!}"
+    local -r content="${2:?content expression not passed to has_characters()}"
+    # shellcheck disable=SC2207
+    local -ra chars=( $(echo "${char_str}" | grep -o .) )
+    local found="false"
+
+    if [[ "$content" == *["$char_str"]* ]]; then
+        debug "has_characters" "does have some of these characters: '${char_str}'"
+        return 0
+    else
+        debug "has_characters" "does NOT have any of these characters: '${char_str}'"
+        return 1
+    fi
+}
+
+function is_bound() {
+    local -n __test_by_ref=$1 2>/dev/null || { debug "is_bound" "unbounded ref";  return 1; }
+    local -r by_val="${1}:-"
+    local name="${!__test_by_ref}" 2
+    local -r arithmetic='→+-=><%'
+    if has_characters "${arithmetic}" "$1"; then
+        debug "is_bound" "${name} is NOT bound"
+        return 1
+    else
+        local idx=${!1} 2>/dev/null 
+        local a="${__test_by_ref@a}" 
+
+        if [[ -z "${idx}${a}" ]]; then
+            debug "is_bound" "${name} is NOT bound: ${idx}, ${a}"
+            return 1
+        else 
+            debug "is_bound" "${name} IS bound: ${idx}, ${a}"
+            return 0
+        fi
+    fi
+}
+
+
+# starts_with <look-for> <content>
+function starts_with() {
+    local -r look_for="${1:?No look-for string provided to starts_with}"
+    local -r content="${2:-}"
+
+    if is_empty "${content}"; then
+        debug "starts_with" "starts_with(${look_for}, "") was passed empty content so will always return false"
+        return 1;
+    fi
+
+    if [[ "${content}" == "${content#"$look_for"}" ]]; then
+        debug "starts_with" "false (\"${DIM}${look_for}${RESET}\")"
+        return 1; # was not present
+    else
+        debug "starts_with" "true (\"${DIM}${look_for}${RESET}\")"
+        return 0; #: found "look_for"
+    fi
+}
+
+# os
+# 
+# Will try to detect the operating system of the host computer
+# where options are: darwin, linux, windowsnt, 
+function os() {
+    local -r os_type=$(lc "${OSTYPE}") || "$(lc "$(uname)")" || "unknown"
+    case "$os_type" in
+        'linux'*)
+           echo "linux"
+          ;;
+        'freebsd'*)
+          echo "freebsd"
+          ;;
+        'windowsnt'*)
+          echo "windows"
+          ;;
+        'darwin'*) 
+          echo "macos"
+          ;;
+        'sunos'*)
+          echo "solaris"
+          ;;
+        'aix'*) 
+          echo "aix"
+          ;;
+        *) echo "unknown/${os_type}"
+    esac
+}
+
+function is_os() {
+  local -r test="${1:?test value for is_os is missing}"
+
+  if [[ "$(os)" == "${test}" ]]; then 
+    return 0;
+  else 
+    return 1;
+  fi
+}
+
+# file_exists <filepath>
+#
+# tests whether a given filepath exists in the filesystem
+function file_exists() {
+    local filepath="${1:?filepath is missing}"
+
+    if [ -f "${filepath}" ]; then
+        return 0;
+    else
+        return 1;
+    fi
+}
+
+# validates that the current directory has a package.json file
+function has_package_json() {
+    local -r filepath="./package.json"
+
+    if [ -f "${filepath}" ]; then
+        return 0;
+    else
+        return 1;
+    fi
+}
+
+function is_keyword() {
+    local _var=${1:?no parameter passed into is_array}
+    local declaration=""
+    # shellcheck disable=SC2086
+    declaration=$(LC_ALL=C type -t $1)
+
+    if [[ "$declaration" == "keyword" ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+# contains <find> <content>
+# 
+# given the "content" string, all other parameters passed in
+# will be looked for in this content.
+function contains() {
+    local -r find="${1}"
+    local -r content="${2}"
+
+    if is_empty "$find"; then
+        error "contains("", ${content}) function did not recieve a FIND string! This is an invalid call!" 1
+    fi
+
+    if is_empty "$content"; then
+        debug "contains" "contains(${find},"") received empty content so always returns false"
+        return 1;
+    fi
+
+    if [[ "${content}" =~ ${find} ]]; then
+        debug "contains" "found: ${find}"
+        return 0 # successful match
+    fi
+
+    debug "contains" "not found: ${find}"
+    return 1
+}
+
+
+# is_empty() <test | ref:test>
+# 
+# tests whether the <test> value passed in is an empty string (or is unset)
+# and returns 0 when it is empty and 1 when it is NOT.
+function is_empty() {
+    local -n __ref__=$1 2>/dev/null
+
+    if is_bound __ref__; then
+        if is_array __ref__; then
+            if [[ ${#__ref__[@]} -eq 0 ]]; then
+                debug "is_empty" "found an array with no elements so returning true"
+                return 0
+            else
+                debug "is_empty" "found an array with some elements so returning false"
+                return 1
+            fi
+        elif is_assoc_array __ref__; then
+            if [[ ${#!__ref__[@]} -eq 0 ]]; then
+                debug "is_empty" "found an associative array with no keys so returning true"
+                return 0
+            else
+                debug "is_empty" "found an associative array with some key/values so returning false"
+                return 1
+            fi
+        else
+            local -r try_pass_by_val="$__ref__" 2>/dev/null
+            if [ -z "$try_pass_by_val" ] || [[ "$try_pass_by_val" == "" ]]; then
+                debug "is_empty" "was empty, returning 0/true"
+                return 0
+            else
+                debug "is_empty" "was NOT empty, returning 1/false"
+                return 1
+            fi
+        fi
+
+    else 
+        if [ -z "$1" ] || [[ "$1" == "" ]]; then
+            debug "is_empty(${1})" "was empty, returning 0/true"
+            return 0
+        else
+            debug "is_empty(${1}))" "was NOT empty, returning 1/false"
+            return 1
+        fi
+    fi
+    
+}
+
+
+# not_empty() <test>
+# 
+# tests whether the <test> value passed in is an empty string (or is unset)
+# and returns 0 when it is NOT empty and 1 when it is.
+function not_empty() {
+    if [ -z "$1" ] || [[ "$1" == "" ]]; then
+        debug "not_empty" "WAS empty, returning 1/false"
+        return 1
+    else
+        debug "not_empty" "was not empty [${#1} chars], returning 0/true"
+        return 0
+    fi
+}
+
+# get_file() <filepath>
+#
+# Gets the content from a file at the given <filepath>
+function get_file() {
+    local -r filepath="${1:?get_file() called but no filepath passed in!}"
+    
+    if file_exists "${filepath}"; then
+        debug "get_file(${filepath})" "getting data"
+        local content
+        { IFS= read -rd '' content <"${filepath}";}  2>/dev/null
+        printf '%s' "${content}"
+    else
+        debug "get_file(${filepath})" "call to get_file(${filepath}) had invalid filepath"
+        return 1
+    fi
+}
+
+
+# tests whether a given string exists in the package.json file
+# located in the current directory.
+function in_package_json() {
+    local find="${1:?find string missing in call to in_package_json}"
+    local -r pkg="$(get_file "./package.json")"
+
+    if contains "${find}" "${pkg}"; then
+        return 0;
+    else
+        return 1;
+    fi
+}
+
+function not_in_package_json() {
+    local find="${1:?find string missing in call to not_in_package_json}"
+    local -r pkg="$(get_file "./package.json")"
+
+    if contains "${find}" "${pkg}"; then
+        return 1;
+    else
+        return 0;
+    fi
+}
+
+# determine which "npm based" package manager to use
+function choose_pkg_manager() {
+    if file_exists "./pnpm-lock.yaml"; then
+        echo "pnpm"
+    elif file_exists "./package-lock.json"; then
+        echo "npm"
+    elif file_exists "./yarn.lock"; then
+        echo "yarn"
+    else
+        echo "pnpm"
+    fi
+}
+
+function npm_install_devdep() {
+    local pkg="${1:?no package sent to npm_install_devdep}"
+    local -r mgr="$(choose_pkg_manager)"
+
+    if in_package_json "\"$pkg\":"; then
+        log "- ${BOLD}${pkg}${RESET} already installed"
+    else 
+        log ""
+        if "${mgr}" install -D "${pkg}"; then
+            log ""
+            log "- installed ${BOLD}${GREEN}${1}${RESET} ${ITALIC}using${RESET} ${mgr}"
+        else
+            log "- problems installing ${RED}${1}${RESET}"
+        fi
+    fi
+
+}
