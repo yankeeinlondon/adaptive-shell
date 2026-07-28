73bb494f8e2de9bea3eec17f3deffb83ec6e7422
Ken Snyder <ken@ken.net>
2026-07-28 09:46:34 -0700

    fix(aliases): force eza hyperlinks on with --hyperlink=always
    - Switch the eza ls/la/ll/ld/lt aliases from bare --hyperlink to --hyperlink=always
    - Bare flag only hyperlinks on a TTY, so output was unlinked when piped; =always makes it consistent

--- reports/aliases.sh
+++ reports/aliases.sh
@@ -74,11 +74,11 @@ function aliases_for_env() {
 
     if has_command "eza"; then
         aliases+=(
-            "ls" "eza --icons=always --hyperlink"
-            "la" "eza -a --icons=always --hyperlink"
-            "ll" "eza -lhga --git --hyperlink --group"
-            "ld" "eza -lDga --git --hyperlink"
-            "lt" "eza -lTL 3 --icons=always --hyperlink"
+            "ls" "eza --icons=always --hyperlink=always"
+            "la" "eza -a --icons=always --hyperlink=always"
+            "ll" "eza -lhga --git --hyperlink=always --group"
+            "ld" "eza -lDga --git --hyperlink=always"
+            "lt" "eza -lTL 3 --icons=always --hyperlink=always"
         )
     elif has_command "exa"; then
         aliases+=(
